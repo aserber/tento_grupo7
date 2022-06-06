68ec8c2452f7f35f4754de2ad35521d6c637d1bc
aserber <alan.serber01@gmail.com>
2022-06-06 13:13:47 -0300

    .

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log('Servidor corriendo en el puerto 3000');
+    console.log('Servidor corriendo en el puerto', PORT);
 })
 
 app.get('/', function(req, res){
