f2e1e42a444fe67cfd2916c00362e65efa304430
Cami0807 <camilacaracciolo@hotmail.com>
2022-06-12 12:18:33 -0300

    ejs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 
 let app = express();
-
+app.set ("view engine","ejs")
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
