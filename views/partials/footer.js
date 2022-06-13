50cbaa46f58c68f1be69527a221a3ffa61873788
Cami0807 <camilacaracciolo@hotmail.com>
2022-06-13 19:38:04 -0300

    partials primera parte

---
+++ views/partials/footer.js
@@ -0,0 +1,20 @@
+
+<footer>
+    <div class="redessociales">
+
+        <a href="https://www.facebook.com/tento.chocolates" target="_blank"><img primerafoto text-align="center"
+                src="/images/facebook.png" alt="" height="25px" width="25px"></a>
+        <a href="https://www.instagram.com/tento.chocolates/" target="_blank"><img primerafoto text-align="center"
+                src="/images/instagram.png" alt="" height="25px" width="25px"></a>
+        <a href="https://api.whatsapp.com/send?phone=541131663482" target="_blank"><img primerafoto text-align="center"
+                src="/images/whatsapp.png" alt="" height="25px" width="25px"></a>
+    </div>
+    <section>
+        <p class="footer2">Politica de calidad</p>
+        <p class="footer2">Bases y condiciones</p>
+        <p class="footer2">Preguntas frecuentes</p>
+        <p class="powered">powered by grupo 7</p>
+    </section>
+</footer>
+
+</html>
---
+++ views/partials/header.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="/css/styles.css">
+    <script src="https://kit.fontawesome.com/7f31ea2864.js" crossorigin="anonymous"></script>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
+    <title>Tentó</title>
+</head>
+<header>
+    <nav>
+        <a href="/index">Inicio</a>
+        <a href="/registro">Registrate</a>
+        <a href="/login">Acceder</a>
+        <a href="/producto">Chocolates</a>
+        <a href="/productostortas">Pasteleria</a>
+        <a href="/compras">Mis Compras</a>
+        <a href="/carrito"><i class="fa-solid fa-cart-shopping"></i></a>
+    </nav>
+    <section class="textos-header">
+        <h1>tentó</h1>
+    </section>
+    <div class="wave" style="height: 400px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+            style="height: 100%; width: 100%;">
+            <path d="M0.00,49.98 C150.00,150.00 349.20,-50.00 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+                style="stroke: none; fill: #d0ab7a;"></path>
+        </svg></div>
+
+</header>
