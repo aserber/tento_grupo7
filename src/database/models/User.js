f0c78d0d9f6c5e7f6a3918b0ae1ca34473c6d3cb
Cami0807 <camilacaracciolo@hotmail.com>
2022-08-14 19:17:38 -0300

    modelo usuarios

---
+++ src/database/models/User.js
@@ -0,0 +1,53 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'user';
+    let cols = {
+        id: {
+            type: dataTypes.INT(10).UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: dataTypes.VARCHAR(100),
+            allowNull: false
+        },
+        lastName: {
+            type: dataTypes.VARCHAR(400),
+            allowNull: false
+        },
+        email: {
+            type: dataTypes.text(50),
+            allowNull: false
+        },
+        image: {
+            type: dataTypes.TINYINT(10).UNSIGNED
+        },
+        password: {
+            type: dataTypes.VARCHAR(10),
+        },
+        id_usercategory: {
+            type: dataTypes.INT(10),
+            allowNull: false
+        }
+    };
+    let config = {
+        timestamps: false,
+    }
+    const user = sequelize.define(alias, cols, config); 
+
+    user.associate = function (models) {
+        user.hasMany(models.sale, { // models.Movie -> Movies es el valor de alias en movie.js
+            as: "sale",
+            foreignKey: 'id_user'
+        })
+    }
+
+    user.associate = function (models) {
+        user.manytoOne(models.userCategory, { // models.Movie -> Movies es el valor de alias en movie.js
+            as: "userCategory",
+            foreignKey: 'id_userCategory'
+        })
+    }
+
+
+    return user
+};
