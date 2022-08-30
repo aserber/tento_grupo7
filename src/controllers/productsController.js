3f6b69d407c05092dbe8bee39b2b7b97fd0c1c84
Cami0807 <camilacaracciolo@hotmail.com>
2022-08-30 16:59:19 -0300

    productos por categoria

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -24,65 +24,34 @@ const controller = {
 	//},
 
 
-	//productCategory:  (req, res) => {
-	//	let categoria = req.params.categoria
-	//	let Chocolate = db.Product.findAll(function (products) {
-	//		products.category =='Chocolate'
-	//	})
-	//	const Pasteleria =  db.Product.findAll(function (products) {
-	//		 products.category =='Pasteleria'
-	//	})
-	//	db.Products.findOne(product => product.category == categoria)
-	//	Promise
-	//	.all([Chocolate, Pasteleria])
-	//	.then (([Chocolate, Pasteleria]) => {
-	//		res.render('productos/producto', 
-	//			{ Pasteleria, Chocolate, toThousand })
-	//		.catch(error => res.send(error))
-	//	})
-	//	
-//
-	//},
-	
 	productCategory: (req, res) => {
-		let categoria = req.params.categoria
-		db.Product.findAll({include:[{association:'productCategory'}]})
-			.then(product => {
-				let Chocolate = product.filter(row =>{
-					return row.id_productcategory == 1
-				})
-				const Pasteleria = product.filter(row => {
-					return row.id_productcategory == 2
-				})
-			})
-			.then(products => {
-				products = {where: {categoria : id_productcategory}}
-				res.render('productos/producto',{products}
-				)
-			})
-
-			
+		let categoria = req.params.category
+		
+		db.Product.findAll(categoria= id_productcategory)
+			.then(product => {	
 				
-
-			
+				res.render('productos/producto', {
+					product,
+					toThousand
+				})
+			})
 	},
 
-	
 	detail: (req, res) => {
 		db.Product.findByPk(req.params.id,
-			)
-			.then(product =>{
-				if (product == undefined){
-				res.redirect('/admin/error')
-			} else{
-				res.render('productos/detail', {
-				product,
-				toThousand
-			})
-			}
-		});
+		)
+			.then(product => {
+				if (product == undefined) {
+					res.redirect('/admin/error')
+				} else {
+					res.render('productos/detail', {
+						product,
+						toThousand
+					})
+				}
+			});
 	},
-	
+
 }
 
 module.exports = controller;
