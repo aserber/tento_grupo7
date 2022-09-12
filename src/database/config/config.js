99f54db84627ab3430dbd65717e71e8a64f0085a
Cami0807 <camilacaracciolo@hotmail.com>
2022-09-12 16:36:22 -0300

    alwaysdata ok menos crear usuario

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "tento",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port":process.env.DB_PORT,
+    "dialect": "mysql",
+    "operatorsAliases": 0
   },
   "test": {
     "username": "root",
@@ -14,10 +18,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "operatorsAliases": 0
   }
 }
