772c702c469ba9a8e6ee4b733aec5265df82ef97
aserber <alan.serber01@gmail.com>
2022-09-18 19:23:49 -0300

    .

--- dashboard/src/components/SideBar.js
+++ dashboard/src/components/SideBar.js
@@ -21,7 +21,7 @@ function SideBar(){
                 <li className="nav-item active">
                     <a className="nav-link" href="/">
                         <i className="fas fa-fw fa-tachometer-alt"></i>
-                        <span>Dashboard - DH movies</span></a>
+                        <span>Dashboard - Tentó</span></a>
                 </li>
 
                 {/*<!-- Divider -->*/}
