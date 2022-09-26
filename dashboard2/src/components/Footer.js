84664fe687d265d579099ec2bb1d985d8e6ee58c
aserber <alan.serber01@gmail.com>
2022-09-26 16:26:06 -0300

    .

--- dashboard2/src/components/Footer.js
+++ dashboard2/src/components/Footer.js
@@ -3,7 +3,7 @@ import image from '../assets/images/grupo7.png';
 function Footer(){
     return (
         <React.Fragment>
-			<footer className="sticky-footer bg-white">
+			<footer className="sticky-footer bg-black">
 				<div className="container my-auto">
 					<div className="copyright text-center my-auto">
 						<span>Copyright &copy; <img className="w-101" src={image} alt="Digital House"/> </span>
