a42b4af85144015ca240c84a4dfa5fd366c4d06f
aserber <alan.serber01@gmail.com>
2022-09-28 22:37:33 -0300

    .

--- src/middlewares/validationProducts.js
+++ src/middlewares/validationProducts.js
@@ -2,7 +2,7 @@ const { body } = require("express-validator");
 const fs = require("fs");
 
 module.exports = [
-    body("name").notEmpty().withMessage("El nombre no puede estar vacio!").isLength({min:2, max:40}).withMessage("El nombre debe tener entre 5 y 40 caracteres"),
+    body("name").notEmpty().withMessage("El nombre no puede estar vacio!").isLength({min:5, max:40}).withMessage("El nombre debe tener entre 5 y 40 caracteres"),
     body("price").notEmpty().withMessage("El precio no puede estar vacio!").isFloat({min:1, max:10000}).withMessage("Coloca el precio que desees."),
     body("description").notEmpty().withMessage("La descripcion no puede estar vacia!").isLength({min: 20}).withMessage("La descripcion debe tener minimo 20 caracteres"),
     body("image").custom((value, {req}) => {
